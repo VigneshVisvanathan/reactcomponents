0b7a6e1ab09a1b81cd394a8bed7939156fa8640c
VigneshVisvanathan <59162869+VigneshVisvanathan@users.noreply.github.com>
2020-06-15 23:26:20 +0530

    'componentsadd'

---
+++ src/components/EventBind.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+class EventBind extends Component {
+constructor(props) {
+    super(props)
+
+    this.state = {
+         message:'Hello'
+    }
+
+    // this.clickHandler=this.clickHandler.bind(this)
+}
+
+// clickHandler(){
+//     this.setState({
+//         message:'GoodBye!'
+//     })
+//     console.log(this);
+    
+// }
+
+clickHandler=()=>{
+    this.setState({
+        message:'GoodBye4!'
+    })
+    console.log(this);
+    
+}
+
+
+    render() {
+        return (
+            <div>
+                <div>{this.state.message}</div>
+               {/* <button onClick={this.clickHandler.bind(this)}>Click EventBind</button> */}
+               {/* <button onClick={()=>this.clickHandler()}>Click EventBind</button>   */}
+               <button onClick={this.clickHandler}>Click EventBind</button>  
+            </div>
+        )
+    }
+}
+
+export default EventBind
